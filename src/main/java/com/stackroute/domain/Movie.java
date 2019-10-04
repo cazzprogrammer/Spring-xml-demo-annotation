34bd9a03bc886a256edd40a385f1835758c669b0
cazzprogrammer <dminj97@gmail.com>
2019-10-04 12:11:57 +0530

    First commit - SpringcoreXML aware-interface using Annotation

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,17 +1,23 @@
 package com.stackroute.domain;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Movie {
-    @Autowired
+public class Movie  implements ApplicationContextAware , BeanFactoryAware, BeanNameAware {
+
     private Actor getActor;
 
-
-    public Movie(Actor getActor)
+    @Autowired
+    public Movie(Actor actor)
     {
-        this.getActor = getActor;
+        this.getActor = actor;
     }
 
 
@@ -28,5 +34,20 @@ public class Movie {
     public String toString()
     {
         return "Movie :" + getActor;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println(applicationContext);
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println(beanFactory);
+    }
+
+    @Override
+    public void setBeanName(String s) {
+        System.out.println("Bean : " + s);
     }
 }
